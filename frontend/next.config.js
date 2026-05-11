da59f4e4ceed29f3126858f551fc71c01c35523a
SystemInfomation <forsyth@ahscampus.com>
2026-05-11 13:55:02 -0400

    Update next.config.js
    Signed-off-by: SystemInfomation <forsyth@ahscampus.com>

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/file',
   images: {
     unoptimized: true
-  },
-  trailingSlash: true
+  }
 }
 
 module.exports = nextConfig
